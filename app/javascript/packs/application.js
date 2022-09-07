2e3287d1b26fc861842b2432fc3cac25f9bb353f
harujamu <tnagisa1220@gmail.com>
2022-09-07 23:46:11 +0000

    [Add]Fontawesomeインストール

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,12 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+// 追加
 import "jquery";
 import "popper.js";
 import "bootstrap";
 import "../stylesheets/application" 
+import '@fortawesome/fontawesome-free/js/all'
 
 Rails.start()
 Turbolinks.start()
