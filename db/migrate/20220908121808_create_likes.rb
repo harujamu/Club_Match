9d1132b9c033d65e11f8636695ec2d0f8e47ea97
harujamu <tnagisa1220@gmail.com>
2022-09-08 12:23:22 +0000

    [Add]Likeモデル・コントローラー作成、アソシエーション記述

---
+++ db/migrate/20220908121808_create_likes.rb
@@ -0,0 +1,11 @@
+class CreateLikes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :likes do |t|
+
+
+      t.integer :recruit_id, null: false
+      t.integer :user_id, null: false
+      t.timestamps
+    end
+  end
+end
