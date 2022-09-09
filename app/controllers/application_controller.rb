276f9bc350562622efc44d5d9276b56127448869
harujamu <tnagisa1220@gmail.com>
2022-09-09 13:15:59 +0000

    [Add]User新規登録時の条件追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+   before_action :configure_permitted_parameters,
+  if: :devise_controller?
+protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,keys:[:club_name, :captain_last_name, :captain_fitst_name, :prefecture, :municipality, :address, :age_group, :genre_id])
+  end
 end
