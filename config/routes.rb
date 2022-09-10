cfd9adfd8aab83312a270af0218052d36dd164e4
harujamu <tnagisa1220@gmail.com>
2022-09-10 12:11:09 +0000

    [Add]namespaceでルーティング管理

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
+  
+  # 管理者ログイン画面
   devise_for :admin,skip:[:passwords, :registrations], controllers:{
     sessions: "admin/sessions"
   }
+  # ユーザーログイン・新規登録画面
   devise_for :users,skip:[:passwords], controllers:{
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
+  # ユーザー画面のルーティング設定
+  
+  
+  # 管理者画面のルーティング設定
+  namespace :admin do
+    
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
