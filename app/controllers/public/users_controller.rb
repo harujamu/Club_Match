51afed8db681555508917df8ccf06b5098da765a
harujamu <tnagisa1220@gmail.com>
2022-09-11 13:18:32 +0000

    [Add]Userコントローラ作成

---
+++ app/controllers/public/users_controller.rb
@@ -0,0 +1,29 @@
+class Public::UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+  end
+  
+  def unsubscribe_confirm
+    @user = User.find(params[:id])
+  end
+  
+  def unsubscribe
+    @user = User.find(params[:id])
+  end
+  
+  private
+  
+  def users_parameter
+    params.require(:user).permit(:id, :club_name, :captain_last_name, :captain_first_name, :age_group, :genre_id, :prefecture, :municipality, :address, :introduction)
+  end
+  
+end
