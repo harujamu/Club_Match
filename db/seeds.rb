b5247dde238f066a53edfe1d036497172d9aa831
harujamu <tnagisa1220@gmail.com>
2022-09-11 06:03:55 +0000

    [Add]管理者登録

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 管理者権限の実装
+Admin.create!(
+  email:'admin@com',
+  password:'admin1'
+  )
