e87bcbc9651d76f04125e4822730eaed4c4539f8
harujamu <tnagisa1220@gmail.com>
2022-09-11 12:37:26 +0000

    [Add]Genre管理画面作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   if: :devise_controller?
 protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,keys:[:club_name, :captain_last_name, :captain_fitst_name, :prefecture, :municipality, :address, :age_group, :genre_id])
+    devise_parameter_sanitizer.permit(:sign_up,keys:[:club_name, :captain_last_name, :captain_first_name, :prefecture, :municipality, :address, :age_group, :genre_id])
   end
 end
