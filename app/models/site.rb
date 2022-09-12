74165ac1a159c9103dfbced39381bb7add3d55fc
harujamu <tnagisa1220@gmail.com>
2022-09-12 08:08:53 +0000

    [Add]Siteモデル編集

--- app/models/site.rb
+++ app/models/site.rb
@@ -4,9 +4,9 @@ class Site < ApplicationRecord
                      gifu: 20, shizuoka: 21, aichi: 22, mie: 23, shiga: 24, kyoto: 25, osaka: 26, hyogo: 27, nara: 28, wakayama: 29,
                      tottori: 30, shimane: 31, okayama: 32, hiroshima: 33, yamaguchi: 34, tokushima: 35, kagawa: 36, ehime: 37, kochi: 38, fukuoka: 39,
                      saga: 40, nagasaki: 41, oita: 42, kumamoto: 43, miiyazaki: 44, kagoshima: 45, okinawa: 46 }
-                     
+
 
   belongs_to:user
   # , dependent: :destroy必要？論理削除でも必要かどうか調べる
- 
+
 end
