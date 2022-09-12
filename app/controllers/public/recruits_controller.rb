98bddcdd9f4f7b41d40f961db9c944af2f0a30f9
harujamu <tnagisa1220@gmail.com>
2022-09-12 08:33:18 +0000

    [Add]募集画面、募集情報編集画面作成

--- app/controllers/public/recruits_controller.rb
+++ app/controllers/public/recruits_controller.rb
@@ -1,2 +1,37 @@
 class Public::RecruitsController < ApplicationController
+  
+  def new
+    @user = current_user
+    @recruit = Recruit.new
+  end
+  
+  def create
+    @recruit = Recruit.new(recruit_params)
+    @recruit.save
+    redirect_to show_recruit_path(@recruit.id)
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    @recruit = Recruit.find(params[:id])
+    @recruit.update(recruit_params)
+    redirect_to show_recruit_path(@recruit.id)
+  end
+  
+  private
+  
+  def recruit_params
+    params.require(:recruit).permit(:id, :user_id, :site_id, :date, :title, :practice_type, :detail, :age_group, :recruit_status, :open_status)
+  end
+  
+  
+  
 end
