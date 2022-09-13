4731d2e4a7169eb1650e9ce8f5edeed05a3c1036
harujamu <tnagisa1220@gmail.com>
2022-09-13 00:31:58 +0000

    [Add]練習場所管理画面作成

--- app/controllers/public/sites_controller.rb
+++ app/controllers/public/sites_controller.rb
@@ -1,2 +1,24 @@
 class Public::SitesController < ApplicationController
+  
+  def create
+    @site = Site.new(site_params)
+    @site.save
+    redirect_to sites_path
+  end
+  
+  def index
+    @site = Site.new
+    @sites = Site.all
+  end
+  
+  def destroy
+    @site = Site.find(params[:id])
+  end
+  
+  private
+  
+  def site_params
+    params.require(:site).permit(:prefecture, :municipality, :address, :id, :user_id)
+  end
+  
 end
