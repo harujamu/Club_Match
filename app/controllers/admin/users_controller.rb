d44f5410fdc80d6560cd696178207a0dd8fc2a34
harujamu <tnagisa1220@gmail.com>
2022-09-13 10:27:19 +0000

    [Add]管理者ユーザー管理画面作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,6 +4,19 @@ class Admin::UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to admin_show_users_path(@user.id)
+  end
+  
+  
+  private
+  
   def user_params
     params.require(:user).permit(:id, :club_name, :captain_last_name, :captain_first_name, :age_group, :genre_id, :prefecture, :municipality, :address, :introduction)
   end
