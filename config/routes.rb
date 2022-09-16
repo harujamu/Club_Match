6de063b7d5fc35e5ee577e78f20b40b6dcedfe56
harujamu <tnagisa1220@gmail.com>
2022-09-16 13:02:39 +0000

    [Fix]管理者用のルーティングも変更

--- config/routes.rb
+++ config/routes.rb
@@ -19,20 +19,12 @@ Rails.application.routes.draw do
   get "/about" => "homes#about", as: "about"
   
   # Recruitsコントローラ
-  # get "/recruits" => "recruits#index"
-  # get "recruits/new" => "recruits#new"
-  # post "/recruits" => "recruits#create", as: "create_recruits"
-  # get "recruits/:id" => "recruits#show", as: "show_recruits"
-  # get "recruits/:id/edit" => "recruits#edit", as: "edit_recruits"
-  # patch "recruits/:id" => "recruits#update", as: "update_recruits"
+  
   resources :recruits, :except => :destroy do
-     # Likesコントローラ
-    # post "recruit/likes" => "likes#create", as: "recruit_likes"
-    # delete "recruit/likes" => "likes#destroy", as: "recruit_destroy_likes"
+  # Likesコントローラ
     resource :likes, :only => [:create, :destroy]
   end
     
-  
   # Usersコントローラ
    get "users/my_page" => "users#my_page" , as: "my_page"
    get "users/:id/edit" => "users#edit", as: "edit_user"
@@ -42,29 +34,18 @@ Rails.application.routes.draw do
    patch "users/unsubscribe/:id" => "users#unsubscribe", as: "user_unsubscribe"
   
   # Entriesコントローラ
-  # get "/entries" => "entries#index"
-  # post "entries/:id" => "entries#create", as: "create_entries"
-  # get "entries/:id" => "entries#show", as: "show_entries"
-  # delete "entries/:id" => "entries#destroy", as: "destroy_entries"
   resources :entries, :only => [:index, :create, :show, :destroy]
   
   # Notifiesコントローラ
-  # get "users/:user_id/notifies" => "notifies#index", as: "notifies"
   resources :notifies, :only => :index
   
   # Sitesコントローラ
-  # get "/sites" => "sites#index"
-  # post "/sites" => "sites#create", as: "create_sites"
-  # delete "sites/:id" => "sites#destroy" , as: "destroy_sites"
   resources :sites, :only => [:index, :create, :destroy]
   
   # Messagesコントローラ
-  # post "messages" => "messages#create", as: "create_messages"
   resources :messages, :only => :create
   
   # Roomsコントローラ
-  # get "rooms/:id" => "room#show", as: "show_romms"
-  # post "rooms" => "rooms#create"
   resources :rooms, :only => [:show, :create]
   
  
@@ -76,14 +57,16 @@ Rails.application.routes.draw do
     get "/" => "homes#top"
     
     # Genresコントローラ
-    get "/genres" => "genres#index"
-    post "/genres" => "genres#create"
-    get "/genres/:id/edit" => "genres#edit", as: "edit_genres"
-    patch "/genres/:id" =>"genres#update", as: "update_genres"
+    # get "/genres" => "genres#index"
+    # post "/genres" => "genres#create"
+    # get "/genres/:id/edit" => "genres#edit", as: "edit_genres"
+    # patch "/genres/:id" =>"genres#update", as: "update_genres"
+    resources :genres, :only => [:index, :create, :edit, :update]
     
     # Uersコンロローラ
-    get "users/:id" => "users#show", as: "show_users"
-    get "users/:id/edit" => "users#edit", as: "edit_users"
-    patch "users/:id" => "users#update", as: "update_users"
+    # get "users/:id" => "users#show", as: "show_users"
+    # get "users/:id/edit" => "users#edit", as: "edit_users"
+    # patch "users/:id" => "users#update", as: "update_users"
+    resources :users, :only => [:show, :edit, :update]
   end
 end
