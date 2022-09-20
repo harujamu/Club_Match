9fbcaea1fc60b5346aa55b15d5d84da5eae3ff2e
harujamu <tnagisa1220@gmail.com>
2022-09-20 11:57:34 +0000

    [Add]ユーザー退会後の処理作成

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-
+  before_action :er_state, only: [:create]
   # GET /resource/sign_in
   # def new
   #   super
@@ -18,15 +18,23 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-def guest_sign_in
-  user = User.guest
-  sign_in user
-  redirect_to root_path, notice: 'ゲストユーザーとしてログインしました'
-end
+  def guest_sign_in
+    user = User.guest
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました'
+  end
+    
+    
   
+  protected
   
-
-  # protected
+  def user_state
+    @user = User.find_by(email: params[:user][:email], club_name: params[:user][:club_name], captain_last_name: params[:user][:captain_last_name], captain_first_name: prams[:user][:captain_first_name])
+    return if !@user
+    if @user.valid_password?(params[:user][:password]) && @user.active_status == false
+      redirect_to new_user_registration_path and return
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
