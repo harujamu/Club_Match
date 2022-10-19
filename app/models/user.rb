4b1a6decad204cca6dd30659c056ec5811658d70
harujamu <tnagisa1220@gmail.com>
2022-10-19 02:31:03 +0000

    [Fix]ユーザーモデルのバリデーション編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,6 @@ class User < ApplicationRecord
   validates :captain_first_name, presence: true
   validates :captain_last_name, presence: true
   validates :introduction, allow_blank: true, length: { maximum: 300 }
-  validates :active_status, presence: true
   validates :age_group, presence: true
   validates :email, uniqueness: true, presence: true
   validates :encrypted_password, uniqueness: true, presence: true
